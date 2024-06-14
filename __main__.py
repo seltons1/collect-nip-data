715da5e2e3e7d427c3fdd6ee56d6784489fc57c2
elton.santos <seltons1@gmail.com>
2024-06-14 12:03:54 -0300

    Reading file method and download improvements.

--- __main__.py
+++ __main__.py
@@ -1,11 +1,16 @@
 import wget
 import os
 import datetime
+import pandas as pd
 
 
 URL = "https://dadosabertos.ans.gov.br/FTP/PDA/demandas_dos_consumidores_nip/"
 
 YEAR = datetime.datetime.now().year
+
+RAW_PATH = 'raw/'
+
+SILVER_PATH = 'silver/'
 
 FILE_NAME = f"""PDA_NIP_{YEAR}.csv"""
 
@@ -23,12 +28,47 @@ FILE_LIST = ['PDA_NIP_2011.csv',
              'PDA_NIP_2022.csv',
              'PDA_NIP_2023.csv']
 
-def download_file(file):
+def download_file(download_type):
 
-    wget.download(URL, file)
+    if download_type == 'ACTUAL_YEAR':
+
+        wget.download(URL+FILE_NAME, RAW_PATH+FILE_NAME)
+
+    elif download_type == 'PREVIOUSLY_YEARS':
+        
+        for file in FILE_LIST:
+            
+            wget.download(URL+file, RAW_PATH+file)
+
+    else:
+
+        print("Command not recognized")
 
     return True
 
+def read_file(file):
+
+    # Defining field types
+    dtype = {"NUMERO_DA_DEMANDA" : 'int64',
+         "ANO_DE_REFERENCIA" : 'int64',
+         "MES_DE_REFERENCIA": 'int64',
+         "CLASSIFICACAO_DA_NIP": 'string',
+         "NATUREZA_DA_NIP": 'string',
+         "CODIGO_OPERADORA": 'string',
+         "NOME_OPERADORA": 'string'
+       }
+
+    # Reading file with pandas
+    df = pd.read_csv(file, sep=';', on_bad_lines='skip', encoding='latin', dtype=dtype)
+
+    # selecting just needed columns
+    filtred_df = df[['NUMERO_DA_DEMANDA', 'ANO_DE_REFERENCIA', 'MES_DE_REFERENCIA', 'CLASSIFICACAO_DA_NIP', 'NATUREZA_DA_NIP','CODIGO_OPERADORA','NOME_OPERADORA']]
+    
+    return filtred_df
+
 if __name__ == "__main__":
 
-    download_file(FILE_NAME)
+    # 
+    download_file('PREVIOUSLY_YEARS')
+
+    print(read_file(RAW_PATH + FILE_NAME).head())
